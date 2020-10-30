ef6988baf7c43c0a21c2fce24c405ef6fd05aecd
Cristian Escamilla <cristianescamilla@outlook.com>
2020-10-30 17:21:49 -0300

    Se crea el componente de clientes
    Se cre el componente de clientes con una lista de valores en formato json el cual se aplica en la vista html por medio de la directiva ngFor

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Cristian', apellido:'Escamilla', email:'cristian@proyecto.com', createAt:'2020-10-30'},
+    {id: 2, nombre: 'Rob', apellido:'Zombie', email:'rob@proyecto.com', createAt:'2019-06-24'},
+    {id: 3, nombre: 'Kayla', apellido:'Yoshikage', email:'kayla@proyecto.com', createAt:'2015-10-30'},
+    {id: 4, nombre: 'Camila', apellido:'Perez', email:'camila@proyecto.com', createAt:'2020-03-30'},
+    {id: 5, nombre: 'Tom', apellido:'Holland', email:'tom@proyecto.com', createAt:'2003-11-22'},
+    {id: 6, nombre: 'Rambo', apellido:'Goldman', email:'rambo@proyecto.com', createAt:'2017-02-13'},
+    {id: 7, nombre: 'Gary', apellido:'McFly', email:'gary@proyecto.com', createAt:'2007-05-21'},
+    {id: 8, nombre: 'Paula', apellido:'Ramirez', email:'paula@proyecto.com', createAt:'2020-01-26'},
+    {id: 9, nombre: 'Zara', apellido:'Vasquez', email:'zara@proyecto.com', createAt:'2006-03-11'},
+    {id: 10, nombre: 'Mike', apellido:'Hilton', email:'mike@proyecto.com', createAt:'2017-09-05'},
+    {id: 11, nombre: 'Ariel', apellido:'Hopkins', email:'ariel@proyecto.com', createAt:'2014-07-12'}
+  ];
